eb8a52a80173286eb38219e5d50fe71a78fc158a
Nikita Petukhov <nedoumok99@gmail.com>
2016-08-02 18:28:08 -0400

    Socket almost working

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ var app = express();
 var morgan = require('morgan');
 var swig = require('swig');
 var routes = require('./routes/')
+var socketio = require('socket.io');
 
 app.use(morgan('dev'));
+var server = app.listen(3000);
+var io = socketio.listen(server)
+console.log('server listening');
 
-
-app.use('/', routes);
+app.use('/', routes(io));
 
 app.set('views', __dirname + '/views'); // point res.render to the proper directory
 app.set('view engine', 'html'); // have res.render work with html files
@@ -15,5 +18,5 @@ app.engine('html', swig.renderFile); // when giving html files to res.render, te
 swig.setDefaults({
   cache: false
 });
-app.listen(3000);
-console.log('server listening');
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser')
 // could use one line instead: var router = require('express').Router();
 var tweetBank = require('../tweetBank');
 
+module.exports = function (io) {
+
+router.use(bodyParser.urlencoded())
+router.use(bodyParser.json())
 router.get('/', function (req, res) {
   var tweets = tweetBank.list();
-  res.render( 'index', { tweets: tweets } );
+  res.render( 'index', { tweets: tweets,showForm: true } );
 });
+
+router.post('/tweets', function(req,res) {
+  console.log(req)
+  var name = req.body.name;
+  var text = req.body.text;
+  tweetBank.add(name,text);
+  io.sockets.emit('newTweet', {name:name, content:text})
+  res.redirect('/')
+})
 
 router.get('/tweets/:id', function(req, res) {
 	var id = req.params.id;
@@ -18,12 +32,13 @@ router.get('/tweets/:id', function(req, res) {
 router.get('/users/:name', function(req, res) {
   var name = req.params.name;
   var tweets = tweetBank.find( {name: name} );
-
-  res.render( 'index', { tweets: tweets } );
+  console.log(typeof tweets[0].name)
+  res.render( 'index', { tweets: tweets ,showForm: true,username: name} );
 });
 
 router.use(express.static('public'));
+return router
+}
 
 
-module.exports = router;
 
